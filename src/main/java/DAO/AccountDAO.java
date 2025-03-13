46d4437b1a3e517dd642207fef2b944c9a51f7c4
Laurence Taylor <morfeus06388casa@gmail.com>
2025-03-13 03:28:17 +0000

    Project-Social-Media-V02

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,6 +1,59 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
 public class AccountDAO {
     
+    public Account insertAccount (Account account){
+        // Open Connection
+        Connection conn = ConnectionUtil.getConnection();
+        // SQL statement
+        String sql = "INSERT INTO Account (username, password) VALUES (?,?);";
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultset = preparedStatement.getGeneratedKeys();
+            if (pkeyResultset.next()){
+                int generatedAccountId =  (int) pkeyResultset.getLong(1);
+                return new Account(generatedAccountId, account.getUsername(),account.getPassword());
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Account getAccountUserName (String username) {
+        Connection conn = ConnectionUtil.getConnection();
+        String sql = "SELECT (username, password) FROM Account where username = ?;";
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+            preparedStatement.setString(1, username);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account account = new Account(rs.getString("username"), rs.getString("password"));
+                return account;
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        }
+        return null;
+    }
     
 }
--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,40 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Model.Account;
+import Model.Message;
+import Util.ConnectionUtil;
+
 public class MessageDAO {
+
+    public Message insertMessage (Message message){
+        // Open Connection
+        Connection conn = ConnectionUtil.getConnection();
+        // SQL statement
+        String sql = "INSERT INTO Message (posted_by, message_text, time_posted_epoch) VALUES (?,?,?);";
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            
+            preparedStatement.setInt(1, message.getPosted_by());
+            preparedStatement.setString(2, message.getMessage_text());
+            preparedStatement.setLong(3, message.getTime_posted_epoch());
+
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultset = preparedStatement.getGeneratedKeys();
+            if (pkeyResultset.next()){
+                int generatedMessageId =  (int) pkeyResultset.getLong(1);
+                return new Message(generatedMessageId, message.getUsername(),account.password);
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
     
 }
